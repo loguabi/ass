279cf52f65d72ee94f7aaa686221457ff84b11b9
logitha <loga848.456@gmail.com>
2016-07-02 15:40:52 +0530

    Create ass.c

---
+++ ass.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int i=123,j=2,k=3,l=1,n,t;
+scanf("%d",&i);
+printf("%d is a seed of :");
+n=i*j*k*l;
+t=n;
+printf("%d is seed of %d bz %d =%d",i,t,n,t);
+return 0;
+}
